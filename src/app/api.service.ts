b9beb8fa8df40eb6f3cecd9be3b5596f69f12ed5
Eric Kramer <theerickramer@gmail.com>
2019-07-26 10:48:47 -0400

    gets pets with hard coded zip

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  API_KEY = environment.apiKey;
+
   constructor(private httpClient: HttpClient) {}
+
+  public getPets() {
+    // tslint:disable-next-line:max-line-length
+    const url = `https://api.rescuegroups.org/v5/public/animals/search/available/cats/haspic?include=breeds,colors,fosters,locations,orgs,patterns,pictures,species,videos,videourls&fields[animals]=name,sex&fields[orgs]=name,email,url,facebookUrl,adoptionUrl&fields[breeds]=name&fields[colors]=name&fields[fosters]=name,email&fields[locations]=name&fields[patterns]=name&fields[pictures]=large,small&fields[species]=singular&fields[videos]=url&fields[videourls]=url,urlThumbnail&options=meta&limit=10`;
+    const data = {
+      filters: [],
+      filterRadius: {
+        miles: 100,
+        postalcode: 90210
+      }
+    };
+    const options = {
+      headers: new HttpHeaders({
+        Authorization: this.API_KEY,
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.httpClient.post(url, data, options);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = environment.apiKey;
+export class AppComponent implements OnInit {
+  title = 'Petr';
+  constructor(private apiService: ApiService) {}
+  ngOnInit() {
+    this.apiService.getPets().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
