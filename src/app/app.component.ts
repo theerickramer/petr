2f2b59b7a4e7c24f940420918ac06cc5bf5ca3de
Eric Kramer <theerickramer@gmail.com>
2019-07-31 13:55:25 -0400

    refactors into getZip()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,35 +14,46 @@ export class AppComponent implements OnInit {
 
   constructor(private apiService: ApiService) {}
   public getPicUrl(picturesData: any[]) {
+    const placeholder =
+      'https://vignette.wikia.nocookie.net/warriorcatsclanroleplay/images/f/fc/Placeholder-pet.png';
     if (picturesData.length > 0) {
       const picId = picturesData[0].id;
       const pic = this.included.find(include => include.id === picId);
       return pic.attributes.large.url;
     }
-    return 'https://vignette.wikia.nocookie.net/warriorcatsclanroleplay/images/f/fc/Placeholder-pet.png';
+    return placeholder;
+  }
+  public getZip() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(position => {
+        let zip = 90210;
+        const { latitude, longitude } = position.coords;
+        const geocoder = new google.maps.Geocoder();
+        const latlng = new google.maps.LatLng(latitude, longitude);
+        geocoder.geocode({ location: latlng }, (results, status) => {
+          if (String(status) === 'OK') {
+            zip = parseInt(
+              results
+                .find(result => result.types.includes('postal_code'))
+                .address_components.find(component =>
+                  component.types.includes('postal_code')
+                ).short_name,
+              10
+            );
+          } else {
+            reject(status);
+          }
+          resolve(zip);
+        });
+      });
+    });
   }
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition(position => {
-      let zip = 90210;
-      const { latitude, longitude } = position.coords;
-      const geocoder = new google.maps.Geocoder();
-      const latlng = new google.maps.LatLng(latitude, longitude);
-      geocoder.geocode({ location: latlng }, (results, status) => {
-        if (String(status) === 'OK') {
-          zip = parseInt(
-            results
-              .find(result => result.types.includes('postal_code'))
-              .address_components.find(component =>
-                component.types.includes('postal_code')
-              ).short_name,
-              10
-          );
-        }
-        this.apiService.getPets(zip).subscribe(response => {
-          console.log(response);
-          this.pets = response.data;
-          this.included = response.included;
-        });
+    this.getZip().then(zip => {
+      this.apiService.getPets(zip).subscribe(response => {
+        console.log(response);
+        this.pets = response.data;
+        this.included = response.included;
       });
     });
   }
